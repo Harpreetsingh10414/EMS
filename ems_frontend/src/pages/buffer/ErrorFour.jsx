77fb3be080c492db954dde2a5943cc442672c863
iKartiK-10 <tbikk545@gmail.com>
2025-04-22 18:27:15 +0530

    frontendtried and worked

---
+++ ems_frontend/src/pages/buffer/ErrorFour.jsx
@@ -0,0 +1,10 @@
+const ErrorFour = () => {
+    return (
+        <>
+         <h1>Meri Galti Hai Maro mujhe Maroo</h1>
+        </>
+       
+    )
+}
+
+export default ErrorFour;
---
+++ ems_frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Dashboard = () => {  
+    return ( 
+        <div>Dashboard Components</div>
+    )
+
+}
+
+export default Dashboard;
+ 
